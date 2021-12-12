9a9f4730c3ccfd4269143066cc5230d8ac86199b
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-12 19:46:28 +0530

    Added: to check year is a leap year or not

---
+++ IF-ElseProgram/LeapYear.js
@@ -0,0 +1,21 @@
+var prompt=require('prompt-sync')();
+year = prompt("Input a Year : ");
+
+if (year % 4 !== 0 && year % 400 !== 0)
+ {
+
+console.log(year + " is not a leap year. Sorry bruh.");
+
+} 
+else if (year % 100 !== 0)
+{
+
+console.log(year + " is a leap year");
+
+} 
+else 
+{
+
+console.log(year + " it is indeed a leap year!");
+
+}
